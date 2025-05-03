3c240057415682a40fd6339204e5da400f80fc5d
HamidReza <ndr.hamid78@gmail.com>
2025-05-03 16:39:43 +0330

    add borg singleton

--- creational/singleton/borg_singleton.py
+++ creational/singleton/borg_singleton.py
@@ -1,4 +1,7 @@
 class BorgSingletonClass:
-    share_state = {}
+    _shared_state = {}
 
     def __new__(cls, *args, **kwargs):
+        obj = super(BorgSingletonClass, cls).__new__(cls, *args, **kwargs)
+        obj.__dict__ = cls._shared_state
+        return obj
